49f3ddf6b411d3dea4ca9b3decfd9f81431f928c
Dmitriy Safronov <zimniy@cyberbrain.pw>
2025-04-02 15:05:19 +0400

    refactor: 404
    Signed-off-by: Dmitriy Safronov <zimniy@cyberbrain.pw>

--- assets/404/404.js
+++ assets/404/404.js
@@ -1,10 +1,6 @@
 //дожидаемся полной загрузки страницы
 window.onload = function () {
 
-    // var timer = setTimeout(function() {
-    //     window.location='https://cyberbrain.pw'
-    // }, 10000);
-
     window.onclick = () => {
         window.location = '/'
     }
